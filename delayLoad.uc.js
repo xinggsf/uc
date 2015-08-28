4a6dd21e5fb5992b69584a5bd569b8dae37a32d0
xinggsf <xinggsf@gmail.com>
2015-08-28 18:32:43 +0800

    Create delayLoad.uc.js
    delayLoad firefox addon

---
+++ delayLoad.uc.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name           delayLoad.uc
+// @description    延时加载FX扩展
+// @include        chrome://browser/content/browser.xul
+// @updateURL      https://github.com/xinggsf/uc/raw/master/delayLoad.uc.js
+// @compatibility  Firefox 34.0+
+// @author         modify by xinggsf
+// @version        2015.8.28
+// ==UserScript==
+
+location == "chrome://browser/content/browser.xul" && (() => {        
+	let {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} =Components;
+	Cu.import("resource://gre/modules/AddonManager.jsm");
+
+	function toggleDelay(disable) {
+		//{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d} ABP
+		['{fe272bd1-5f76-4ea4-8501-a05d35d823fc}',//ABE
+		 'firebug@software.joehewitt.com'
+		].forEach(id => AddonManager.getAddonByID(id, 
+			addon => addon.userDisabled = disable));
+	}
+
+	//启用 延迟加载扩展
+	this._timer && clearTimeout(this._timer);
+	this._timer = setTimeout(() => toggleDelay(false), 1500);
+
+	// firefox关闭时禁用 延迟加载扩展
+	window.addEventListener("unload", () => toggleDelay(true), false);
+})();
