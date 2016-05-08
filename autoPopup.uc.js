56ec7d62feffbdedbd9b7b0ce299e941dcbdbc12
xinggsf <xinggsf@gmail.com>
2016-05-08 10:30:27 +0800

    Signed-off-by: xinggsf <xinggsf@gmail.com>

--- autoPopup.uc.js
+++ autoPopup.uc.js
@@ -4,348 +4,308 @@
 // @updateURL      https://raw.githubusercontent.com/xinggsf/uc/master/autoPopup.uc.js
 // @namespace      autoPopup@gmail.com
 // @include        chrome://browser/content/browser.xul
-// @compatibility  Firefox 34.0+
-// @author         GOLF-AT, xinggsf
-// @version        2016.4.27
-//以下所注释内容由xinggsf改进
+// @compatibility  Firefox 45+
+// @author         xinggsf
+// @version        2016.5.8
+// 以下所注释内容由xinggsf改进
+// @note           2016.5.8 OOP封装：以清晰、简单的逻辑，真正实现定时自动弹出和关闭菜单
 // @note           2016.1.2限定脚本作用在FX内部
 // @note           2015.12.10用setter控制，防止弹出多个菜单;新增多个omnibar内的图标白名单
 // @note           2015.11.8取消书签的自动弹出，因为我的任务栏也是左竖栏
 // @note           2015.8.8增加白名单功能，用之处理omnibar内的多个图标
 // ==/UserScript==
 
--function (doc) {
-	let nDelay = 290;
-	let _overElt = null;
-	let popElt = null;
-	let popTimer = null;
-	let hideTimer = null;
-	let searchBar = null;
-
-	//by xinggsf,支持Fx的CSS所有语法: #表示id，. 表示class
-	let blackIDs = ['.bookmark-item'];
-
-	//by xinggsf, 白名单，及触发动作
-	let whiteIDs = [{
-	//放在omnibar中的搜索引擎图标
-		id: 'omnibar-defaultEngine',
-		popMemu: 'omnibar-engine-menu',
-		/*run: function(overElem){
-			$('omnibar-in-urlbar').click(0);
-		}*/
-	},
-	{
-		id: 'SimpleMusicPlayer',
-		popMemu: 'SimpleMusicPlayer-popup',
-		run: null
-	},
-	{
-		id: 'uAutoPagerize-icon',
-		popMemu: 'uAutoPagerize-popup',
-	},
-	{//弹出页面信息窗口
-		id: 'verticaltoolbar-page-info-button',
-		run: function(overElem){
-			BrowserPageInfo();
-		}
-	},
-	{
-		id: 'readLater',
-		popMemu: 'readLater-popup',
-		//function(overElem){ popElt.popup();}
-	},
-	{
-		id: 'foxyproxy-toolbar-icon',
-		popMemu: 'foxyproxy-toolbarbutton-popup',
-	}];
-	let whitesInx = -1;
-
-	let popupPos = ['after_start', 'end_before', 'before_start', 'start_before'];
-
-	let menuPanelID = 'PanelUI-popup';
-	let downPanelID = 'downloadsPanel';
-	let widgetPanelID = 'customizationui-widget-panel';
-
-	let ppmContainer = $('mainPopupSet');
-	function $(id) {
-		return doc.getElementById(id);
-	}
-	
-	function isWidgetBtn(elt) {
-		try {
-			return elt.hasAttribute('widget-id')
-				&& elt.getAttribute('widget-type') === 'view';
-		} catch (e) {
-			return false;
+function clone(src){//浅克隆
+	if (!src) return src;
+	let r = src.constructor === Object ?
+		new src.constructor() :
+		new src.constructor(src.valueOf());
+	for (let key in src){
+		if ( r[key] !== src[key] ){
+			r[key] = src[key];
 		}
 	}
+	r.toString = src.toString;
+	r.valueOf = src.valueOf;
+	return r;
+}
+function $(id) {
+	return document.getElementById(id);
+}
+const nDelay = 260,
+//禁止自动弹出。CSS语法: #表示id，. 表示class
+blackIDs = ['#back-button','#forward-button'];//'.bookmark-item',
+//by xinggsf, 白名单，及触发动作
+let whiteIDs = [{
+//放在omnibar中的搜索引擎图标
+	id: 'omnibar-defaultEngine',
+	popMemu: 'omnibar-engine-menu',
+	//run: overEle => $('omnibar-in-urlbar').click(0)
+}, {
+	id: 'ublock0-button',
+	popMemu: 'ublock0-panel',
+	run: overEle => overEle.click()
+}];
+//let ppmContainer = $('mainPopupSet');
 
-	function isSearchBtn(elt) {
-		try {
-			return whitesInx === 0 ||
-			elt.getAttribute("anonid") === 'searchbar-search-button';
-		} catch (e) {
-			return false;
-		}
+class MenuAct {//菜单动作基类－抽象类
+	//btn = null;ES6不支持实例属性这样定义
+	//ppm = null;
+	constructor() {
+		if (new.target === MenuAct)
+			throw new Error('MenuAct类不能实例化');
 	}
-
-	function isNewMenuBtn(elt) {
-		try {
-			return elt.id == 'PanelUI-menu-button';
-		} catch (e) {
-			return false;
-		}
+	isButton(e) {//_isButton的装饰方法
+		let r = this._isButton(e);
+		if (!r) return r;
+		this.btn = e;
+		this.ppm = this.getPopupMenu(e);
+		return r;
 	}
-
-	function isDownloadBtn(elt) {
-		try {
-			return elt.localName == 'toolbarbutton'
-			&& elt.id == 'downloads-button';
-		} catch (e) {
-			return false;
-		}
-	}
-
-	function isAutoComplete(elt) {
-		try {
-			return elt.getAttribute('type').startsWith('autocomplete');
-		} catch (e) {
-			return false;
-		}
-	}
-
-	function getPopupMenu(elt) {
-		if (!elt) return null;
-		if (whitesInx > -1 && popElt) return popElt;
-		let s = elt.getAttribute('popup') || elt.getAttribute('context');
+	getPopupMenu(e) {
+		let s = e.getAttribute('context') || e.getAttribute('popup');
 		if (s) return $(s);
-		s = elt.ownerDocument.getAnonymousNodes(elt);
-		for (let k of s) {
-			if (k.localName === 'menupopup')
-				return k;
-		}
-		return null;
+		return e.querySelector('menupopup,menulist');
+		/* let r = /menupopup|menulist/,
+		c = e.ownerDocument.getAnonymousNodes(e);
+		for (let k of c)
+			if (r.test(k.nodeName)) return k; */
 	}
-
-	function isBlackNode(elt) {
-		return blackIDs.some(css => elt.mozMatchesSelector(css));
-	}
-
-	function getPopupPos(elt) {
-		let pos, box, w, h,
-		r = /toolbar|hbox|vbox/,
-		x = elt.boxObject.screenX,
-		y = elt.boxObject.screenY;
-
-		for (pos = 0; elt != null; elt = elt.parentNode)
-		{
-			if (elt.localName == 'window' || !elt.parentNode)
-				break;
-			else if (!r.test(elt.localName))
-				continue;
-			h = elt.boxObject.height;
-			w = elt.boxObject.width;
-			if (h >= 45 && h >= 3 * w) {
-				pos = 9;
-				break;
-			} else if (w >= 45 && w >= 3 * h) {
-				pos = 8;
-				break;
-			}
+	open(){
+		let m = this.ppm;
+		if (m) {
+			if (m.openPopup)
+				m.openPopup(this.btn, null, 15, 15);
+			else if (m.showPopup)
+				m.showPopup();
+			else if (m.popupBoxObject)
+				m.popupBoxObject.showPopup();
+			else this.btn.click();
 		}
-		try {
-			box = elt.boxObject;
-			x = (pos & 1) ? (x <= box.width / 2 + box.screenX ? 1 : 3) :
-				(y <= box.height / 2 + box.screenY ? 0 : 2);
-		} catch (e) {
-			x = 0;
-		}
-		return popupPos[x];
+		else this.btn.click();
 	}
-
-	function getPopupNode(node) {
-		if (whitesInx !== -1 && popElt)
-			return popElt;
-		let elt, s,
-		isPop = !1, //Node isn't Popup node
-		r = /menupopup|popup|menulist/;
-
-		for (; node != null; node = node.parentNode) {
-			if (node == popElt) return node;
-
-			s = node.localName;
-			if (r.test(s) || isAutoComplete(node) || IsMenuButton(node))
-				isPop = true;
-			else if (s == 'dropmarker') {
-				if (node.getAttribute('type') === 'menu') {
-					elt = node.parentNode;
-					isPop = elt.firstChild.localName === 'menupopup';
-				}
-				else if (node.className==='autocomplete-history-dropmarker')
-					isPop = true;
-				else {
-					try {
-						isPop = node.parentNode.id === 'urlbar';
-					} catch (ex) {}
-				}
-			} else if (s == 'menu')
-				isPop = 'menubar' === node.parentNode.localName;
-			else if (IsButton(node)) {
-				for (elt = node; (elt = elt.nextSibling) != null;) {
-					if (elt.localName === 'dropmarker' &&
-						elt.boxObject.width > 0 &&
-						elt.boxObject.height > 0)
-						break;
-				}
-				if (elt) break;
-			}
-			if (isPop) break;
+	close(){
+		if (this.ppm) {
+			if (this.ppm.hidePopup)
+				this.ppm.hidePopup();
+			else if (this.ppm.popupBoxObject)
+				this.ppm.popupBoxObject.hidePopup();
 		}
-		if (popElt && node && popElt.contains(node))
-			return popElt;
-		return isPop ? node : null;
 	}
-
-	function popupMenu(btn, m) {
-		try {
-			let pos = getPopupPos(btn);
-			m.openPopup(btn, pos, 0, 0, false, false, null);
-		} catch (e) {}
-	}
-	function autoPopup() {
-		popTimer = null;
-		if (!_overElt) return;
-
-		_hidePopup();
-
-		if (whitesInx > -1) {
-			if (whiteIDs[whitesInx].run)
-				whiteIDs[whitesInx].run(overElt);
-			else if (popElt)
-				popupMenu(overElt, popElt);
-			return;
+}
+let menuActContainer = [
+	new class extends MenuAct{//处理白名单
+		_isButton(e) {
+			this.idx = e.hasAttribute('id') ?
+				whiteIDs.findIndex(k => k.id === e.id) : -1;
+			return this.idx !== -1;
 		}
-		if (!popElt) popElt = overElt;
-
-		if (overElt.localName == 'dropmarker')
-			popElt.showPopup();
-		else if (overElt.localName == 'menulist')
-			overElt.open = true;
-		else if (isNewMenuBtn(overElt)) {
+		getPopupMenu(e) {
+			let id = whiteIDs[this.idx].popMemu;
+			return id ? $(id) : super.getPopupMenu(e);
+		}
+		open() {
+			let fn = whiteIDs[this.idx].run;
+			fn ? fn(this.btn) : super.open();
+		}
+	}(),
+	new class extends MenuAct{//处理黑名单
+		_isButton(e) {
+			return blackIDs.some(css => e.mozMatchesSelector(css));//matches
+		}
+		open() {}//弹出动作为空从而禁止自动弹出，但可定时关闭菜单
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			return e.localName === 'dropmarker';
+		}
+		close() {
+			this.ppm.parentNode.closePopup();
+		}
+		open() {
+			this.ppm.showPopup();
+		}
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			return e.localName === 'menulist';
+		}
+		getPopupMenu(e) {
+			return this.btn;
+		}
+		close() {
+			this.btn.open = !1;
+		}
+		open() {
+			this.btn.open = !0;
+		}
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			return e.id === 'PanelUI-menu-button';
+		}
+		getPopupMenu(e) {
+			return $('PanelUI-popup');
+		}
+		close() {
+			PanelUI.hide();
+		}
+		open() {
 			PanelUI.show();
-			popElt = $(menuPanelID);
-		} else if (isWidgetBtn(overElt)) {
-			let cmdEvent = doc.createEvent('xulcommandevent');
-			cmdEvent.initCommandEvent("command", true, true, window, 0, false,
-				false, false, false, null);
-			overElt.dispatchEvent(cmdEvent);
-			popElt = $(widgetPanelID);
-		} else if (isDownloadBtn(overElt)) {
-			popElt = $(downPanelID);
+		}
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			return e.id === 'downloads-button';
+		}
+		getPopupMenu(e) {
+			return $('downloadsPanel');
+		}
+		close() {
+			DownloadsPanel.hidePanel();
+		}
+		open() {
 			DownloadsPanel.showPanel();
-		} else if (isSearchBtn(overElt)) {
-			searchBar.openSuggestionsPanel();
-		} else {
-			popElt = getPopupMenu(overElt);
-			popElt ? popupMenu(overElt, popElt) : overElt.click();
+		}
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			return e.hasAttribute('widget-id') &&
+				e.getAttribute('widget-type') === 'view';
+		}
+		open() {
+			this.btn.click();
+		}
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			return e.getAttribute("anonid") === 'searchbar-search-button';
+		}
+		close() {
+			BrowserSearch.SearchBar.textbox.closePopup();
+		}
+		open() {
+			BrowserSearch.SearchBar.openSuggestionsPanel();
+		}
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			return /toolbarbutton|button/.test(e.localName) &&
+				this.getPopupMenu(e);;
+		}
+	}(),
+	new class extends MenuAct{
+		_isButton(e) {
+			try {
+				return e.closest('toolbar') &&
+				('image' === e.nodeName || e.src.startsWith('data:image')) &&
+				this.getPopupMenu(e);
+			} catch (ex) {
+				return false;
+			}
+		}
+	}(),
+];
+class AutoPop {
+	constructor() {
+		this.timer = 0;
+		this.act = null;//MenuAct
+	}
+	clearTimer() {
+		if (this.timer) {
+			clearTimeout(this.timer);
+			this.timer = 0;
 		}
 	}
-
-	function _hidePopup() {
-		try {
-			if (overElt.localName == 'dropmarker')
-				popElt.parentNode.closePopup();
-			else if (overElt.localName == 'menulist')
-				overElt.open = false;
-			else if (isDownloadBtn(overElt))
-				DownloadsPanel.hidePanel();
-			//else if (isNewMenuBtn(overElt) || isWidgetBtn(overElt))
-			else if (popElt && popElt.hidePopup)
-				popElt.hidePopup();
-			else if (popElt.popupBoxObject)
-				popElt.popupBoxObject.hidePopup();
-			else if (isSearchBtn(overElt))
-				 searchBar.textbox.closePopup();
+	clean() {
+		if (!this.act) return;
+		this.clearTimer();
+		this.act = null;
+	}
+}
+let btnManager = new class extends AutoPop {
+	setTimer() {
+		this.timer = setTimeout(() => {
+			this.act.open();
+			ppmManager.clean();
+			ppmManager.act = clone(this.act);
+			this.clean();
+		}, nDelay);
+	}
+	mouseOver(e) {
+		this.clean();
+		//',[type^=autocomplete],.autocomplete-history-dropmarker'
+		if (e.closest('menupopup,menulist,popupset') || e.disabled)
+			return;
+		for (let k of menuActContainer) {
+			if (k.isButton(e)) {
+				this.act = k;
+				this.setTimer();
+				break;
+			}
 		}
-		catch (e) {}
-		finally {hideTimer = null;}
 	}
-
-	function hidePopup() {
-		_hidePopup();
-		_overElt = popElt = null;
+}();
+let ppmManager = new class extends AutoPop {//菜单管理
+	inMenu(e) {
+		let a = this.act;
+		this.isMenu = a && ((a.ppm && a.ppm.contains(e)) ||
+			e.closest('menupopup,menulist,popupset'));//',[type^=autocomplete],.autocomplete-history-dropmarker'
+		return this.isMenu;
 	}
-
-	function mouseOver(ev) {
-		if (!doc.hasFocus()) {
-			_overElt && hidePopup();
+	clean() {
+		if (this.act) {
+			this.act.close();
+			super.clean();
+		}
+	}
+	setTimer() {//定时器司职关闭菜单
+		if (!this.timer) this.timer = setTimeout(() => {
+			this.clean();
+		}, nDelay);
+	}
+	mouseOver(e) {
+		if (this.inMenu(e) || (this.act && e === this.act.btn)) {
+			this.clearTimer();
 			return;
 		}
-		let popNode, e = ev.target;
-		//console.log(e.firstChild);		
-		if (e.closest('menupopup,menulist,popupset')) return;
-		//if ('menuitem' === e.nodeName) return;
-		//if (e.closest('#mainPopupSet')) return;//ppmContainer.contains(e)
-
-		whitesInx = e.hasAttribute('id') ?
-			whiteIDs.findIndex(k => k.id === e.id) : -1;
-		if (whitesInx > -1) {
-			overElt = e;
-			if (whiteIDs[whitesInx].popMemu)
-				popElt = $(whiteIDs[whitesInx].popMemu);
-			popTimer = setTimeout(autoPopup, nDelay);
-			return;
-		}
-
-		popNode = getPopupNode(e);
-		if (isAutoComplete(popNode)) return;
-		if (!popNode || (popNode && popNode.disabled) || isBlackNode(popNode)) {
-			mouseOut();
-			return;
-		}
-
-		if (hideTimer) {
-			window.clearTimeout(hideTimer);
-			hideTimer = null;
-		}
-
-		if (popElt && popNode == popElt && popElt != overElt) return;
-		if (overElt && popNode != overElt) hidePopup();
-		overElt = popNode;
-		popElt = null;
-		popTimer = setTimeout(autoPopup, nDelay);
+		if (this.act) this.setTimer();
 	}
+}();
 
-	function mouseOut(e) {
-		if (popTimer) {
-			clearTimeout(popTimer);
-			popTimer = null;
+function getPopupPos(elt) {
+	let box, w, h,
+	b = !1,
+	x = elt.boxObject.screenX,
+	y = elt.boxObject.screenY;
+
+	while (elt = elt.parentNode.closest('toolbar,hbox,vbox')) {
+		h = elt.boxObject.height;
+		w = elt.boxObject.width;
+		if (h >= 45 && h >= 3 * w) {
+			b = !0;
+			break;
 		}
-		if (!hideTimer && popElt)
-			hideTimer = setTimeout(hidePopup, nDelay);
+		if (w >= 45 && w >= 3 * h) break;
 	}
+	if (!elt) return 'after_start';
+	box = elt.boxObject;
+	x = b ? (x <= w / 2 + box.screenX ? 1 : 3) :
+			(y <= h / 2 + box.screenY ? 0 : 2);
+	return ['after_start','end_before','before_start','start_before'][x];
+}
 
-	function IsButton(elt) {
-		try {
-			return /toolbarbutton|button/.test(elt.localName);
-		} catch (e) {
-			return false;
-		}
+var prevElt = null;
+function mouseOver(ev) {
+	if (!document.hasFocus()) {
+		ppmManager.clean();
+		return;
 	}
+	let elt = ev.target;
+	if (elt === prevElt) return;
+	prevElt = elt;
+	btnManager.mouseOver(elt);
+	ppmManager.mouseOver(elt);
+}
 
-	function IsMenuButton(elt) {
-		return isNewMenuBtn(elt) || isDownloadBtn(elt) || isWidgetBtn(elt)
-			|| (IsButton(elt) && getPopupMenu(elt));
-	}
-
-    this.__defineGetter__("overElt", function(){
-        return _overElt;
-    });
-    this.__defineSetter__("overElt", function(x){
-        if (x === _overElt) return;
-		if (_overElt) _hidePopup();
-		_overElt = x;
-    });
-	searchBar = BrowserSearch.SearchBar;
-	window.addEventListener('mouseover', mouseOver, false);
-}(document);
+window.addEventListener('mouseover', mouseOver, false);
