34f6adacddb3ea7f1a709cea209c8a86e1f3e1c9
xinggsf <xinggsf@gmail.com>
2025-05-30 09:04:33 +0800

    更新 i18n.mjs

--- i18n.mjs
+++ i18n.mjs
@@ -5,19 +5,21 @@ const _LANGS = {
 const _LOCALE = Services.prefs.getCharPref("general.useragent.locale", "zh-CN").split('-')[0];
 const LANG = _LANGS[_LOCALE] || _LANGS.en;
 
-export function format(s, ...values) {
-	let i = 0, key = s.trim().replace(/\s{2,}/g, ' ');
-	if (!(key in LANG)) LANG[key] = s;
+function template(a, ...values) {
+	let i = 0, key = a.join('').trim().replace(/\s{2,}/g, ' ');
+	if (!(key in LANG)) LANG[key] = a.join('%s');
 	return LANG[key].replaceAll('%s', x => values[i++]);
 }
 
-export const LL = new Proxy(format, {
-	apply(target, ctx, args) {
-		args[0] = args[0].join('%s');
-		return Reflect.apply(target, ctx, args);
-	},
+export const LL = new Proxy(template, {
+	// apply(target, ctx, args) {
+		// return Reflect.apply(target, ctx, args);
+	// },
 	get(target, name) {
-		if (name === '_data') return JSON.stringify(LANG);
+		if (name === 'debug') return function() {
+			console.log(JSON.stringify(LANG));
+		}
+		if (name === '_data') return LANG;
 	}
 });
 /*
